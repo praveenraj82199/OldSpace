12b2671e7d85e4134fa20f14d149b82199ae156e
praveenraj82199 <rajpraveen648@gmail.com>
2024-08-21 08:46:33 +0530

    new on august

---
+++ src/org/data/FaceBookLogin.java
@@ -0,0 +1,15 @@
+package org.data;
+
+public class FaceBookLogin {
+	public static void main(String[]arg) {
+		int mobileNo = 6797998;
+		byte age = 20;
+		String email = "jack@123";
+		char grad= 'a';
+		float mark = 98.54545f;
+		System.out.println("myage="+age+"\n"+mobileNo+"\n"+email+"\n"+"grade="+grad);
+		
+		
+	}
+
+}
